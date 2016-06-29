124b96f29a023797beee4e2a44572e00d6b4c7a1
Pascal <meteo66240@free.fr>
2016-06-29 09:09:54 +0200

    Update misc_del.sh
    Dont del cache & secure, just dalvik in each update of the same rom

--- KERNELS/stock_pepper/ramdisk/sbin/misc_del.sh
+++ KERNELS/stock_pepper/ramdisk/sbin/misc_del.sh
@@ -1,8 +1,11 @@
 #!/sbin/sh
 
-find /sdcard/Android -type f -exec rm -rf {} \;
-find /sdcard/Android -type d -exec rm -rf {} \;
-find /sdcard/.android_secure -type f -exec rm -rf {} \;
-find /sdcard/.android_secure -type d -exec rm -rf {} \;
+#Not format Data folder each update
+#find /sdcard/Android -type f -exec rm -rf {} \;
+#find /sdcard/Android -type d -exec rm -rf {} \;
+
+#find /sdcard/.android_secure -type f -exec rm -rf {} \;
+#find /sdcard/.android_secure -type d -exec rm -rf {} \;
+#Let remove Dalvik
 rm -rf /cache/dalvik*
 
